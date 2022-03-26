8f086d8de73867fb74b6ad9e46f63c2f3fbb6bcd
FlyHigh-K <102417687+FlyHigh-K@users.noreply.github.com>
2022-03-26 12:38:29 +0000

    Changing strings

---
+++ Main (1).java
@@ -0,0 +1,14 @@
+class Main {
+  public static void main(String[] args) {
+    String s1="I'm on a seafood diet. I see food and I eat it.";
+    String s2=".";
+    System.out.println(s1.contains(s2));
+    System.out.println(s1.lastIndexOf('d'));
+    System.out.println(s1.substring(17,21));
+    System.out.println(s1.toLowerCase());
+    System.out.println(s1.toUpperCase());
+    System.out.println(s1.trim());
+    System.out.println(s1.indexOf('d'));
+  }
+}
+//read all of the functions from page: https://www.w3schools.com/java/java_ref_string.asp
